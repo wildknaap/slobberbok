403a221e33b41e18e6eeb20114530771dc874d87
wildknaap <eduardwildknaap@gmail.com>
2016-02-08 16:24:28 +0100

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/screen /usr/bin/sudo /usr/sbin/openvpn --config /home/<user>/.vpn/client.ovpn
+/usr/bin/screen /usr/bin/sudo /usr/sbin/openvpn --config /home/<user>/.vpn/client.ovpn --auth-retry interact
 
 #EOF
