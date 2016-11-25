22e49ea156cddbecadb77dc327b90426609a6522
wildknaap <eduardwildknaap@gmail.com>
2016-11-25 23:39:53 +0100

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -2,14 +2,32 @@
 #
 #
 
-SNAPDIR="<dir>"
+EMAIL="<mail>"
+SNAPDIR="<snapshot dir>"
 DATE=`date +%Y%m%d`
-VG="/dev/<vg>"
-LV="<lv>"
+VG="/dev/vg_<volumegroup>"
+LV="<lv_snapshot>"
 
-	/sbin/lvcreate --size 10G --snapshot --name "$i"_snap $VG/$i
-	/bin/dd if=$VG/"$i"_snap | gzip > $SNAPDIR/$i.dd-$DATE.gz
-	/sbin/lvremove -f $VG/"$i"_snap
-	/usr/bin/logger -s "Snapshot of $i completed"
+
+for i in $LV
+
+        do 
+
+        /sbin/lvcreate --size 10G --snapshot --name "$i"_snap $VG/$i
+        /bin/dd if=$VG/"$i"_snap | gzip > $SNAPDIR/$i.dd-$DATE.gz
+        /sbin/lvremove -f $VG/"$i"_snap
+        /usr/bin/logger -s "Snapshot of $i completed"
+
+        if [ $? -eq 0 ]
+
+                then
+                        /usr/bin/logger -s "Snapshot of $i completed"
+                else
+                        `/bin/echo "[SNAPSHOT FAILURE] $LV on $DATE" | /usr/bin/mail -s "[SNAPSHOT FAILURE] $LV on $DATE" $EMAIL`
+
+        fi
+
+done
+
 
 # EOF
