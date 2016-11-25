58ed4720b7a5d552500d604ae4c4a701acf36c8c
wildknaap <eduardwildknaap@gmail.com>
2016-11-25 23:12:33 +0100

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -10,7 +10,6 @@ LV="<lv>"
 	/sbin/lvcreate --size 10G --snapshot --name "$i"_snap $VG/$i
 	/bin/dd if=$VG/"$i"_snap | gzip > $SNAPDIR/$i.dd-$DATE.gz
 	/sbin/lvremove -f $VG/"$i"_snap
-	/bin/rm $SNAPDIR/$i.dd-$DATE
 	/usr/bin/logger -s "Snapshot of $i completed"
 
 # EOF
