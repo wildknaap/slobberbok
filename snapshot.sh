a78b17a35010edeb3d09590b8f47afe727b31c79
wildknaap <eduardwildknaap@gmail.com>
2016-11-25 23:00:00 +0100

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -7,7 +7,7 @@ DATE=`date +%Y%m%d`
 VG="/dev/<vg>"
 LV="<lv>"
 
-  /sbin/lvcreate --size 10G --snapshot --name "$i"_snap $VG/$i
+	/sbin/lvcreate --size 10G --snapshot --name "$i"_snap $VG/$i
 	/bin/dd if=$VG/"$i"_snap | gzip > $SNAPDIR/$i.dd-$DATE.gz
 	/sbin/lvremove -f $VG/"$i"_snap
 	/bin/rm $SNAPDIR/$i.dd-$DATE
