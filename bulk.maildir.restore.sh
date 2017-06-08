33fee4a13f1dda3840fb89000a0e7ee9ad043819
wildknaap <eduardwildknaap@gmail.com>
2017-06-08 10:23:13 +0200

    bulk.maildir.restore.sh

---
+++ bulk.maildir.restore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#
+
+USERS=`cat mailuser.list`
+DOMAIN="<domain>" 
+SRCDIR="/tmp/backup-restores/home/vmail/$DOMAIN/"
+DSTDIR="/home/vmail/$DOMAIN/"
+
+
+for i in $USERS
+
+	do 
+
+	/usr/bin/rsync -av $SRCDIR/$i/Maildir/ $DSTDIR/$i/Maildir	
+
+	/usr/bin/doveadm -Dv index -u $i@$DOMAIN INBOX
+
+	echo "Maildir restore for $i@$DOMAIN completed"	
+	
+done
+
+
+# EOF
